1711739ad1768062ec9c399d6bf803e68e2da08e
Vishal Kumar Singh <codewithvishaldgp@gmail.com>
2026-09-09 12:42:02 +0530

    Create Experiment_7.1.sql

---
+++ Experiment-7/Experiment_7.1.sql
@@ -0,0 +1,29 @@
+DECLARE
+    CURSOR exp7 IS
+        SELECT NAME, SALARY
+        FROM (
+            SELECT NAME, SALARY
+            FROM STAFF
+            ORDER BY SALARY DESC
+        )
+        WHERE ROWNUM <= 5;
+
+    v_name   STAFF.NAME%TYPE;
+    v_salary STAFF.SALARY%TYPE;
+
+BEGIN
+    OPEN exp7;
+
+    LOOP
+        FETCH exp7 INTO v_name, v_salary;
+        EXIT WHEN exp7%NOTFOUND;
+
+        DBMS_OUTPUT.PUT_LINE(
+            'Name: ' || v_name ||
+            ', Salary: ' || v_salary
+        );
+    END LOOP;
+
+    CLOSE exp7;
+END;
+/
