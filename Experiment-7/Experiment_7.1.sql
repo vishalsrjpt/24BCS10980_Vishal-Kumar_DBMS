063c96540a10a7a22cce3648ba4364da21671188
Vishal Kumar Singh <codewithvishaldgp@gmail.com>
2026-09-09 12:43:57 +0530

    Update Experiment_7.1.sql

--- Experiment-7/Experiment_7.1.sql
+++ Experiment-7/Experiment_7.1.sql
@@ -1,29 +1,19 @@
+--TABLE STAFF was bydefault created in ORACLE Free SQL so just wrote the command and skipped full table creation.
 DECLARE
     CURSOR exp7 IS
         SELECT NAME, SALARY
-        FROM (
-            SELECT NAME, SALARY
-            FROM STAFF
-            ORDER BY SALARY DESC
+        FROM( SELECT NAME, SALARY FROM STAFF ORDER BY SALARY DESC
         )
         WHERE ROWNUM <= 5;
-
     v_name   STAFF.NAME%TYPE;
     v_salary STAFF.SALARY%TYPE;
-
 BEGIN
     OPEN exp7;
-
     LOOP
         FETCH exp7 INTO v_name, v_salary;
         EXIT WHEN exp7%NOTFOUND;
-
-        DBMS_OUTPUT.PUT_LINE(
-            'Name: ' || v_name ||
-            ', Salary: ' || v_salary
-        );
+        DBMS_OUTPUT.PUT_LINE('Name: ' || v_name ||', Salary: ' || v_salary);
     END LOOP;
-
     CLOSE exp7;
 END;
 /
